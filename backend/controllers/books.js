873738a32a9e984cb728e89fa8548ff619ac2ba0
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-02-14 16:38:39 +0100

    Projet7_backend book-controllers
    Création du fichier de contrôle pour initialiser et créer les routes nécessaires pour les différentes demandes faites à l'api. Création de la route GET pour tous les livres.

---
+++ backend/controllers/books.js
@@ -0,0 +1,15 @@
+const Book = require('../models/book');
+
+exports.getAllBooks = (req, res, next) => {
+    Book.find().then(
+        (books) => {
+            res.status(200).json(books);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+}
