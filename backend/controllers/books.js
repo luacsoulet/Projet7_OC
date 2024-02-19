b18e01b495aca5c24c57da6aa28207ef0e4d442d
Lucas Magalhaes <lucasmagalhaes45200@gmail.com>
2024-02-20 00:38:14 +0100

    Projet7_backend book controllers update
    Mise à jour du fichier controllers pour les différentes requête des livres en ajoutant la sélection d'un livre par son id.

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,4 +1,21 @@
 const Book = require('../models/book');
+const fs = require('fs');
+
+exports.getOneBook = (req, res, next) => {
+    Book.findOne({
+        _id: req.params.id
+    }).then(
+        (book) => {
+            res.status(200).json(book);
+        }
+    ).catch(
+        (error) => {
+            res.status(401).json({
+                error: error
+            });
+        }
+    );
+};
 
 exports.getAllBooks = (req, res, next) => {
     Book.find().then(
