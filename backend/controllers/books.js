fd7c61349d891fc05980c892d3ac4ae0d8474ea9
Lucas Magalhaes <lucasmagalhaes45200@gmail.com>
2024-02-20 00:40:39 +0100

    Projet7_backend controllers book
    Mise à jour du fichier controllers des requêtes pour les livres en ajoutant la création d'un livre.

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,6 +1,21 @@
 const Book = require('../models/book');
 const fs = require('fs');
 
+exports.createBook = (req, res, next) => {
+    const bookObject = JSON.parse(req.body.book);
+    delete bookObject._id;
+    delete bookObject._userId;
+    const book = new Book({
+        ...bookObject,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}//images/${req.file.filename}`
+    });
+
+    book.save()
+        .then(() => { res.status(201).json({ message: 'Livre enregistré !' }) })
+        .catch(error => { res.status(400).json({ error }) })
+};
+
 exports.getOneBook = (req, res, next) => {
     Book.findOne({
         _id: req.params.id
