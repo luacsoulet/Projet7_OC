e7d29b0831744c2e97d7a629c6a6891b6374b079
Lucas Magalhaes <lucasmagalhaes45200@gmail.com>
2024-02-21 16:13:03 +0100

    Projet7_backend app.js update
    Mise à jour des paths pour la sauvegarde des images.

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/illustrations', express.static('./illustrations'));
 app.use('/api/books', booksRoutes);
 app.use('/api/auth', userRoutes);
 
