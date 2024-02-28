ce197b1807f156cfb479b227a8b9108a49fd30f7
Lucas Magalhaes <lucasmagalhaes45200@gmail.com>
2024-02-28 11:36:49 +0100

    Export
    Exportation du code en cours.

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -2,6 +2,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 const fs = require('fs').promises;
 const path = require('path');
+const { log } = require('console');
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
@@ -9,37 +10,38 @@ const MIME_TYPES = {
     'image/png': 'png'
 };
 
-const compressImage = async (imageName) => {
-    const imagePath = path.join(__dirname, '../illustrations/', imageName);
+// const compressImage = async (imageName) => {
+//     const imagePath = path.join(__dirname, `./illustrations/${imageName}`);
+//     console.log(imagePath);
+//     try {
+//         // Read the image from the specified directory
+//         const inputBuffer = await fs.readFile(`./illustrations/${imageName}`);
+//         console.log(imagePath);
+//         console.log(inputBuffer);
+//         // Compress the image with sharp
+//         const compressedBuffer = await sharp(inputBuffer)
+//             .jpeg({ quality: 50 }) // Set quality to 80% for JPEG images
+//             .toBuffer();
 
-    try {
-        // Read the image from the specified directory
-        const inputBuffer = await fs.readFile(imagePath);
+//         // Save the compressed image back to the same directory
+//         await fs.writeFile(imagePath, compressedBuffer);
 
-        // Compress the image with sharp
-        const compressedBuffer = await sharp(inputBuffer)
-            .jpeg({ quality: 80 }) // Set quality to 80% for JPEG images
-            .toBuffer();
-
-        // Save the compressed image back to the same directory
-        await fs.writeFile(imagePath, compressedBuffer);
-
-        console.log(`Image ${imageName} compressed and saved successfully.`);
-    } catch (error) {
-        console.error(`Error compressing image ${imageName}: ${error.message}`);
-    }
-};
+//         console.log(`Image ${imageName} compressed and saved successfully.`);
+//     } catch (error) {
+//         console.error(`Error compressing image ${imageName}: ${error.message}`);
+//     }
+// };
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
         callback(null, 'illustrations');
     },
     filename: async (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        const name = file.originalname.split(' ').join('-');
         const extension = MIME_TYPES[file.mimetype];
         const ref = name.replace(`.${extension}`, '') + Date.now() + '.' + extension;
-        compressImage(ref);
         callback(null, ref);
+        // compressImage(ref);
     }
 });
 
