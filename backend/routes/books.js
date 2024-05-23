1711b26d3c17069f0a1904d9bad43397ab0313f8
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-05-24 01:42:21 +0200

    Projet7_backend-book-routes.js update
    Ajout des middleware multer uploadBookCover et resizeBookCover au requêtes d'ajout et de modification des livres.

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -9,8 +9,8 @@ const booksCtrl = require('../controllers/books');
 router.get('/', booksCtrl.getAllBooks);
 router.get('/bestrating', booksCtrl.getBestRating);
 router.get('/:id', booksCtrl.getOneBook);
-router.post('/', auth, multer, booksCtrl.createBook);
-router.put('/:id', auth, multer, booksCtrl.modifyBook);
+router.post('/', auth, multer.uploadBookCover, multer.resizeBookCover, booksCtrl.createBook);
+router.put('/:id', auth, multer.uploadBookCover, multer.resizeBookCover, booksCtrl.modifyBook);
 router.post('/:id/rating', auth, booksCtrl.ratingBook);
 router.delete('/:id', auth, booksCtrl.deleteBook);
 
